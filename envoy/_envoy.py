5760a594e46e878bdb8d79e69bdd27750f432fc3
Choko <choko@curioswitch.org>
2025-12-12 21:15:10 +0900

    try it out

--- envoy/_envoy.py
+++ envoy/_envoy.py
@@ -1,7 +1,11 @@
 from __future__ import annotations
 
+import sys
 from pathlib import Path
 
 
 def get_envoy_path() -> Path:
-    return Path(__file__).parent / "_bin" / "envoy"
+    exe = "envoy"
+    if sys.platform == "win32":
+        exe += ".exe"
+    return Path(__file__).parent / "_bin" / exe
